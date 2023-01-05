bb49df89b9171ad295fb0eef009c4a02476403dd
Nachop51 <nachoperalta0@gmail.com>
2023-01-05 17:18:52 +0000

    fixing setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,12 @@ apt-get update
 apt-get install -y hugo make
 apt-get install -y wget
 
+cd /tmp
 wget "https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.tar.gz"
 tar -xf "hugo_0.84.0_Linux-64bit.tar.gz"
 rm /usr/bin/hugo
-mv /tmp/hugo /usr/bin/hugo
+mv ./hugo /usr/bin/hugo
+cd /app
 rm -rf /tmp
 
 make build
