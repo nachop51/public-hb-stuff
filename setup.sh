3b552c812187a7a2a901055849135d575fcbd7c2
Nachop51 <nachoperalta0@gmail.com>
2023-01-05 17:12:26 +0000

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-apt-get update && apt-get install -y hugo make wget
+apt-get update
+apt-get install -y hugo make
+apt-get install -y wget
 
 wget "https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.tar.gz"
 tar -xf "hugo_0.84.0_Linux-64bit.tar.gz"
