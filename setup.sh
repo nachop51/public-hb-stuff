3fd7019bbfb990c26af268e0794c7bcbc68e4fde
Nachop51 <nachoperalta0@gmail.com>
2023-01-05 16:57:32 +0000

    task 4

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt-get update && apt-get install -y hugo make wget
+
+wget "https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.tar.gz"
+tar -xf "hugo_0.84.0_Linux-64bit.tar.gz"
+rm /usr/bin/hugo
+mv /tmp/hugo /usr/bin/hugo
+rm -rf /tmp
+
+make build
