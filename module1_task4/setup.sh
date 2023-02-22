864ec1b733fd3715f2c44872aa15909402abee43
Nachop51 <nachoperalta0@gmail.com>
2023-02-22 13:26:56 -0300

    Fixing the script

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -5,5 +5,6 @@ apt-get install -y make wget
 wget "https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.tar.gz"
 tar -xvf hugo_0.84.0_Linux-64bit.tar.gz hugo
 mv hugo /usr/local/bin/
+rm hugo_extended_0.109.0_Linux-64bit.tar.gz
 
 make build
